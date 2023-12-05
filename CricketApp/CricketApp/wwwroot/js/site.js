39489e36cdbc06841626fc484ac26f894b5d1370
Rajababu Swain <rajababu.swain@prorigosoftware.com>
2023-12-05 17:36:02 +0530

    Added Test Cases projects too

--- CricketApp/CricketApp/wwwroot/js/site.js
+++ CricketApp/CricketApp/wwwroot/js/site.js
@@ -1,84 +1,113 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-//rajababu swain
-// wwwroot/js/site.js
-
-// Import necessary CSS classes for animation
+﻿// Import necessary CSS classes for animation
 const SUCCESS_ANIMATION_CLASS = "animate-success";
 const ERROR_ANIMATION_CLASS = "animate-error";
 
 function toggleAnimation(element, className) {
-  element.classList.add(className);
-  setTimeout(() => {
-    element.classList.remove(className);
-  }, 1000);
+    element.classList.add(className);
+    setTimeout(() => {
+        element.classList.remove(className);
+    }, 1000);
+}
+
+function validateRegistration() {
+    // Get form elements
+    const username = document.getElementById('username');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    const confirmPassword = document.getElementById('confirmPassword');
+    const termsCheckbox = document.getElementById('termsCheckbox');
+    const messageEl = document.getElementById('registrationMessage');
+
+    // Clear any previous errors
+    messageEl.textContent = '';
+
+    // Username validation
+    if (!username.value) {
+        messageEl.textContent = 'Please enter a username.';
+        setDynamicColor(messageEl, "#dc3545");
+        toggleAnimation(messageEl, ERROR_ANIMATION_CLASS);
+        return false;
+    }
+
+    // Email validation
+    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+<span class="math-inline">/\.test\(email\.value\) \) \{
+        messageEl\.textContent \= 'Please enter a valid email address\.';
+setDynamicColor\(messageEl, "\#dc3545"\);
+        toggleAnimation\(messageEl, ERROR\_ANIMATION\_CLASS\);
+        return false;
+        \
+    }
+    // Password validation
+    if \(\!/^\(?\=\.\*\\d\)\(?\=\.\*\[a\-z\]\)\(?\=\.\*\[A\-Z\]\)\.\{8,\}</span > /.test(password.value)) {
+    messageEl.textContent = 'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one number.';
+    setDynamicColor(messageEl, "#dc3545");
+    toggleAnimation(messageEl, ERROR_ANIMATION_CLASS);
+    return false;
+}
+
+// Confirm password validation
+if (confirmPassword.value !== password.value) {
+    messageEl.textContent = 'Passwords do not match.';
+    setDynamicColor(messageEl, "#dc3545");
+    toggleAnimation(messageEl, ERROR_ANIMATION_CLASS);
+    return false;
+}
+
+// Terms checkbox validation
+if (!termsCheckbox.checked) {
+    messageEl.textContent = 'You must agree to the terms of service and privacy policy.';
+    setDynamicColor(messageEl, "#dc3545");
+    toggleAnimation(messageEl, ERROR_ANIMATION_CLASS);
+    return false;
+}
+
+// All validations passed, proceed with registration
+registerUser({
+    username: username.value,
+    email: email.value,
+    password: password.value
+});
+
+return true;
+}
+
+function registerUser(userData) {
+    // Replace "YOUR_API_ENDPOINT" with your actual API endpoint
+    fetch('YOUR_API_ENDPOINT/register', {
+        method: 'POST',
+        body: JSON.stringify(userData),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+            // User registered successfully
+            messageEl.textContent = 'Registration successful! Please check your email for verification.';
+            setDynamicColor(messageEl, "#007bff");
+            toggleAnimation(messageEl, SUCCESS_ANIMATION_CLASS);
+
+            // Clear form fields after successful registration
+            username.value = '';
+            email.value = '';
+            password.value = '';
+            confirmPassword.value = '';
+            termsCheckbox.checked = false;
+        } else {
+            response.json().then(error => {
+                messageEl.textContent = error.message;
+                setDynamicColor(messageEl, "#dc3545");
+                toggleAnimation(messageEl, ERROR_ANIMATION_CLASS);
+            });
+        }
+    })
 }
 
 function validateLogin() {
-  const email = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
-  const loginMessage = document.getElementById("loginMessage");
+    const email = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const loginMessage = document.getElementById("loginMessage");
 
-  // Validate email format
-  if (!isValidEmail(email)) {
-    loginMessage.innerText = "Please enter a valid email address.";
-    setDynamicColor(loginMessage, "#dc3545");
-    toggleAnimation(loginMessage, ERROR_ANIMATION_CLASS);
-    return;
-  }
-
-  // Send AJAX request for authentication
-  fetch("/Login/ValidateCredentials", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ email: email, password: password }),
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Server response was not ok");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      if (data.isSuccess) {
-        // User authentication successful
-        loginMessage.innerText = "Login successful!";
-        setDynamicColor(loginMessage, "#007bff");
-        toggleAnimation(loginMessage, SUCCESS_ANIMATION_CLASS);
-
-        // Redirect to the appropriate page based on user role
-        if (data.userRole === "Admin") {
-          window.location.href = "/Admin/Index";
-        } else if (data.userRole === "User") {
-          window.location.href = "/User/Index";
-        } else {
-          // Handle invalid role
-          showMessage("Invalid user role");
-        }
-      } else {
-        // Invalid credentials or other errors
-        loginMessage.innerText = data.errorMessage || "Invalid credentials. Please try again.";
-        setDynamicColor(loginMessage, "#dc3545");
-        toggleAnimation(loginMessage, ERROR_ANIMATION_CLASS);
-      }
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-      loginMessage.innerText = "An error occurred. Please try again.";
-      setDynamicColor(loginMessage, "#dc3545");
-      toggleAnimation(loginMessage, ERROR_ANIMATION_CLASS);
-    });
-}
-
-function setDynamicColor(element, color) {
-  element.style.color = color;
-}
-
-function isValidEmail(email) {
-  // Implement more robust email validation if needed
-  return email.checkValidity();
-}
+    // Email format validation
+    if (!isValidEmail(email)) {
+        loginMessage.innerText = "Please enter a valid email address.";
+        setDynamicColor(loginMessage, "#dc3545
